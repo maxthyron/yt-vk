21eb99080046c3892fa0f29b41702074ec70765b
Max Thyron <mkthyron@gmail.com>
2019-09-01 22:07:43 +0300

    Init files added

---
+++ api.py
@@ -0,0 +1,70 @@
+from __future__ import unicode_literals
+import vk_api
+import json
+import re
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from vk_api.utils import get_random_id
+from env import auth
+from download import get_audio
+
+def find_yt(text):
+    pattern = r'(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.\S*'
+    links = [l.group() for l in  re.finditer(pattern, text)]
+    return links
+
+def upload_doc(vk, upload, event, path, title):
+    doc = upload.document(path, title=title, tags=[], message_peer_id=event.obj.peer_id)['doc']
+    print(doc)
+    vk.messages.send(user_id=event.obj.from_id, random_id=get_random_id(), attachment=f"doc{doc['owner_id']}_{doc['id']}")
+
+def upload_yt(vk, upload, event, path, title, artist):
+    audio = upload.audio(audio=path,
+                         artist=artist,
+                         title=title)
+    vk.messages.send(user_id=event.obj.from_id,
+            random_id=get_random_id(),
+            attachment=f"audio{audio['owner_id']}_{audio['id']}")
+
+def main():
+    vk_session = vk_api.VkApi(token=auth.KEY)
+    print("Got VK API Session")
+    longpoll = VkBotLongPoll(vk_session, auth.GROUP_ID)
+    print("Got Group Session")
+    vk = vk_session.get_api()
+    print("Got VK API Object")
+
+    audio_session = vk_api.VkApi(login=auth.LOGIN, password=auth.PASSW)
+    try:
+        audio_session.auth(token_only=True)
+    except vk_api.AuthError as error_msg:
+        print(error_msg)
+        return
+    print("Got Audio Session")
+    upload = vk_api.VkUpload(audio_session)
+    uploadd = vk_api.VkUpload(vk_session)
+    print("Got Upload Object")
+
+    print("Start Longpoll listening")
+    for event in longpoll.listen():
+        if event.type == VkBotEventType.MESSAGE_NEW:
+            print("From:", event.obj.from_id)
+            print('Message:', event.obj.text)
+            links = find_yt(event.obj.text)
+            for l in links:
+                result = get_audio(l)                
+                print(result)
+                path, title, artist = result[0]
+                # upload_yt(vk, upload, event, path, title, artist)
+                print("BEFORE")
+                upload_doc(vk, uploadd, event, path, title)
+                print("AFTER")
+        elif event.type == VkBotEventType.MESSAGE_REPLY:
+            print("From(Me):", event.obj.peer_id)
+            print('Message:', event.obj.text)
+            print()
+        else:
+            print(event.type)
+            print()
+
+if __name__ == '__main__':
+    main()
---
+++ download.py
@@ -0,0 +1,75 @@
+import youtube_dl
+import sys
+import re
+
+def get_filename(msg):
+    fpattern = r"storage\/.*\.(wav|mp3|m4a)"
+    fnames = re.finditer(fpattern, msg)
+
+    result = []
+    for name in fnames:
+        result.append([name.group()] + name.group().strip("storage/.mp3").split("---"))
+    return result
+
+class Intercepter():
+    def __init__(self):
+        self.result = None
+        self.type = None
+    def debug(self, msg):
+        if msg.startswith('[ffmpeg] Destination:'):
+            self.result = msg
+        else:
+            print(msg)
+            self.type = 'other'
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+download_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'm4a',
+        'preferredquality': '320',
+    }],
+    'outtmpl': 'storage/%(title)s---%(uploader)s.%(ext)s',
+    'logger': None,
+}
+
+info_opts = {
+    'listformats' : True,
+    'logger': None,
+}
+
+def get_audio(link):
+    inter = Intercepter()
+    download_opts['logger'] = inter
+    try:
+        with youtube_dl.YoutubeDL(download_opts) as ydl:
+            ydl.download([link])
+    except:
+        pass
+    return get_filename(inter.result)
+
+def get_info(link):
+    inter = Intercepter()
+    info_opts['logger'] = inter
+    try:
+        with youtube_dl.YoutubeDL(info_opts) as ydl:
+            ydl.download([link])
+        print(inter.result if inter.result or inter.type == 'other' else "Fail")
+    except:
+        pass
+
+def main():
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'get':
+            get_audio(link)
+        elif sys.argv[1] == 'info':
+            get_info(link)
+
+if __name__ == "__main__":
+    main()
