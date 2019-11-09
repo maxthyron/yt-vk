5c1ba807e8bb6d64bf9290301207f4c46eb5330c
Max Thyron <mkthyron@gmail.com>
2019-11-09 12:20:00 +0000

    Script update

--- run.sh
+++ run.sh
@@ -2,10 +2,5 @@
 
 log_file=logs.txt
 
-nohup python3 -u ./api.py > $log_file &
-pid=$(pgrep -f api.py)
-echo $pid
-date >> $log_file
- >> $log_file
-
-
+nohup python3 -u ./api.py > logs.txt 2>&1 & echo $! > run.pid
+cat run.pid | pgrep -P $!
